8579ef2639b71f3a937334b33a96f5c55cf13011
Nivedh Nambiar <66726103+Nivedh-prog@users.noreply.github.com>
2024-09-24 12:39:39 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const msgerForm = get(".msger-inputarea");
-const msgerInput = get(".msger-input");
-const msgerChat = get(".msger-chat");
+const msgerForm = get(".chat-input-area");
+const msgerInput = get(".chat-input");
+const msgerChat = get(".chat-window");
 
 const BOT_MSGS = [
-"Hi, how are you?",
-"Turn your location on for find people near you",
-"I am powered by ChatGPT 4",
-"I feel sleepy! :("];
-
+  "Hello! How can I assist you today?",
+  "Please enable location services to connect with people nearby.",
+  "I'm powered by ChatGPT 4, ready to help!",
+  "Just here to assist you anytime you need!"
+];
 
 // Icons made by Freepik from www.flaticon.com
 const BOT_IMG = "https://image.flaticon.com/icons/svg/327/327779.svg";
@@ -20,31 +20,29 @@ msgerForm.addEventListener("submit", event => {
 
   const msgText = msgerInput.value;
   if (!msgText) return;
+  
   appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
   msgerInput.value = "";
   botResponse();
 });
 
 function appendMessage(name, img, side, text) {
-  //   Simple solution for small apps
   const msgHTML = `
-    <div class="msg ${side}-msg">
-      <div class="msg-img" style="background-image: url(${img})"></div>
-
-      <div class="msg-bubble">
-        <div class="msg-info">
-          <div class="msg-info-name">${name}</div>
-          <div class="msg-info-time">${formatDate(new Date())}</div>
+    <div class="message ${side}-message">
+      <div class="message-avatar" style="background-image: url(${img})"></div>
+      <div class="message-bubble">
+        <div class="message-info">
+          <div class="message-info-name">${name}</div>
+          <div class="message-info-time">${formatDate(new Date())}</div>
         </div>
-
-        <div class="msg-text">${text}</div>
+        <div class="message-text">${text}</div>
       </div>
     </div>
   `;
-
   msgerChat.insertAdjacentHTML("beforeend", msgHTML);
   msgerChat.scrollTop += 500;
 }
+
 function botResponse() {
   const r = random(0, BOT_MSGS.length - 1);
   const msgText = BOT_MSGS[r];
@@ -54,16 +52,19 @@ function botResponse() {
     appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
   }, delay);
 }
+
 // Utils
 function get(selector, root = document) {
   return root.querySelector(selector);
 }
+
 function formatDate(date) {
   const h = "0" + date.getHours();
   const m = "0" + date.getMinutes();
 
   return `${h.slice(-2)}:${m.slice(-2)}`;
 }
+
 function random(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
